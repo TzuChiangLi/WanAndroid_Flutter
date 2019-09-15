7522dd1349e0484f354679ae5277c664e4044e02
TzuChiangLi <724581911@qq.com>
2019-09-15 15:57:31 +0800

    2019年09月15日15:57:22 1.搜索界面布局微调

--- lib/pages/search/searchPage.dart
+++ lib/pages/search/searchPage.dart
@@ -13,22 +13,29 @@ class _SearchPage extends State<SearchPage> {
       appBar: AppBar(
         title: TextField(
           autofocus: true,
+          style: TextStyle(
+            fontSize: 14,
+          ),
           decoration: InputDecoration(
+            //设置高度
+            contentPadding: EdgeInsets.fromLTRB(16, 6, 0, 6),
             hintText: "请输入关键字",
             hintMaxLines: 1,
             fillColor: Color.fromARGB(255, 230, 231, 237),
             filled: true,
-            border: InputBorder.none,
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide.none),
           ),
         ),
         actions: <Widget>[
           IconButton(
             onPressed: null,
             icon: Image.asset("imgs/search_btn.png"),
-            iconSize: 18,
+            padding: EdgeInsets.fromLTRB(4, 12, 16, 12),
           ),
         ],
-        centerTitle: true,
+        centerTitle: false,
         automaticallyImplyLeading: false,
         elevation: 0.0,
         brightness: Brightness.light,
